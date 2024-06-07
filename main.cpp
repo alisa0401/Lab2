5ad1ba58d8ac8f0fa3481e8240644771114f52f5
alisa <alisa01@snu.ac.kr>
2024-06-07 17:27:35 +0900

    assingment

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
+	if(cnt<N){	       
+		vec.push_back(cnt+1);
+		func(cnt+1);
+		if(vec.size()==M){
+			for(int i=0; i<M; i++){
+				cout<<vec[i]<<" ";
+			}
+			cout<<endl;
+		}
+		vec.erase(vec.end()-1);
+		func(cnt+1);	
+	}
 
 }
 
